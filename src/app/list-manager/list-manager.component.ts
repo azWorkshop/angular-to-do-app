de7c503230ad7609126123e2f4b434c7f70de030
Anna <you@example.com>
2020-11-21 15:24:57 +0100

    Add storage service

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -17,7 +17,7 @@ export class ListManagerComponent implements OnInit {
    }
 
   addItem(title: string) {
-    this.todoList.push({ title });
+    this.todoListService.addItem({ title });
   }
 
 }
